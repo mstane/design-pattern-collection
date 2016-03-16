2136ca3f61d16465f2f5ac2beb124b5856820bdd
Stanislav Markov <mstane@gmail.com>
2016-03-16 23:07:56 +0000

    Support for serializing Java objects with XStream.

---
+++ common/src/main/java/org/sm/dpc/common/ObjectSerializer.java
@@ -0,0 +1,49 @@
+package org.sm.dpc.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thoughtworks.xstream.XStream;
+
+public class ObjectSerializer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ObjectSerializer.class);
+	
+	protected static XStream xstream = new XStream();
+	
+	private ObjectSerializer() {
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T loadObjectFromXml(String fileName) {
+		InputStream inputStream = null;
+		Object object = null;
+		try {
+			inputStream = ObjectSerializer.class.getClassLoader().getResourceAsStream(fileName);
+			if (inputStream == null) {
+				logger.error("File not found.");
+				return null;
+			}
+			byte[] data = new byte[inputStream.available()];
+			inputStream.read(data);
+			object = xstream.fromXML(new String(data));
+		} catch (IOException e) {
+			logger.error("", e);
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+			}
+		}
+		return (T)object;
+	}
+	
+
+}
